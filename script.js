536424042d3ba3eec2263868c4b873deb4750a22
SSH User <ssh-357278-mp@portia.ispgateway.de>
2014-09-16 13:48:15 +0200

    Deutsche Uebersetzung und Option zur Auswahl von Sonderzeichen

--- script.js
+++ script.js
@@ -1,19 +1,21 @@
-function genpw(id,plen,arg1,arg2,arg3,arg4){
+function genpw(id,plen,arg1,arg2,arg3,arg4,arg5){
     obj=document.getElementById(id);
     arg1=document.getElementById(arg1);
     arg2=document.getElementById(arg2);
     arg3=document.getElementById(arg3);
     arg4=document.getElementById(arg4);
-    obj.value=GeneratePassword(plen,arg1.checked,arg2.checked,arg3.checked,arg4.checked);
+    arg5=document.getElementById(arg5);
+    obj.value=GeneratePassword(plen,arg1.checked,arg2.checked,arg3.checked,arg4.checked,arg5.value);
 }
 
-function GeneratePassword(length,arg1,arg2,arg3,arg4) {
+function GeneratePassword(length,arg1,arg2,arg3,arg4,arg5) {
     var res = '';
     var str='';
     var str1='qwertyuioplkjhgfdsazxcvbnm';
     var str2='QWERTYUIOPLKJHGFDSAZXCVBNM';
     var str3='1234567890';
-    var str4='!@#$%^&*.,';
+//    var str4='!@#$%^&*.,';
+    var str4= arg5
 
     if(arg1){ str=str+str1; }
     if(arg2){ str=str+str2; }
